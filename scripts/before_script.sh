fbdf3a6d09f71b3f38e6d612223e0e559a7f793f
ci-prow-krishvoor <85279874+ci-prow-krishvoor@users.noreply.github.com>
2022-01-17 16:47:26 +0530

    Update before_script.sh

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env sh
 
 # Download kubectl
-- curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.20.1/bin/linux/ppc64le/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.20.1/bin/linux/ppc64le/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 # Download minikube.
-- curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.20.0/minikube-linux-ppc64le && chmod +x minikube && sudo mv minikube /usr/local/bin/
-- mkdir -p $HOME/.kube $HOME/.minikube
-- touch $KUBECONFIG
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.20.0/minikube-linux-ppc64le && chmod +x minikube && sudo mv minikube /usr/local/bin/
+mkdir -p $HOME/.kube $HOME/.minikube
+touch $KUBECONFIG
