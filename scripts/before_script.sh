9c66216b527672b678504170592b80d29e28f118
ci-prow-krishvoor <85279874+ci-prow-krishvoor@users.noreply.github.com>
2022-01-17 16:42:18 +0530

    Create before_script.sh

---
+++ scripts/before_script.sh
@@ -0,0 +1,8 @@
+
+# Download kubectl
+- curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.20.1/bin/linux/ppc64le/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+# Download minikube.
+- curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.20.0/minikube-linux-ppc64le && chmod +x minikube && sudo mv minikube /usr/local/bin/
+- mkdir -p $HOME/.kube $HOME/.minikube
+- touch $KUBECONFIG
